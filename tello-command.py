a77745fea11f16b8aedc4a1ecee43463691068cf
doffing81 <doffing81@gmail.com>
2021-05-15 15:35:32 -0600

    Created tello-command.py

---
+++ tello-command.py
@@ -0,0 +1,20 @@
+import socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+tello_addr = ("192.168.10.1", 8889)
+sock.bind(("", 9000))
+
+while True:
+    try:
+        msg = input("")
+        if not msg:
+            break
+        if "end" in msg:
+            sock.close()
+            break
+        msg = msg.encode()
+        print(msg)
+        sent = sock.sendto(msg, tello_addr)
+    except Exception as err:
+        print(err)
+        sock.close()
+        break
